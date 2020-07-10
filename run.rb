12c7f50fb184a4ad2d8b95239a40e1a6ee6e8067
Dmitry Sychev <axiumone@gmail.com>
2020-07-09 20:17:36 -0400

    setup file structure

---
+++ run.rb
@@ -0,0 +1,17 @@
+require_relative '../config/environment'
+
+# api = GetDealerInfo.new
+# api.local_search
+cli = CommandLineInterface.new
+
+print "
+                                                                               
+#               #####                       
+#       ###### #     #  ####   ####   ####  
+#       #      #       #    # #    # #    # 
+#       #####  #  #### #    # #    # #      
+#       #      #     # #    # #    # #  ### 
+#       #      #     # #    # #    # #    # 
+####### ######  #####   ####   ####   ####  
+"
+cli.client_greeting
